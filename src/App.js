90870980f801101f963b7b589623fc553a916be4
Shemo <moshe.frydmann@gmail.com>
2021-12-21 21:59:34 +0200

    ajout footer

--- src/App.js
+++ src/App.js
@@ -13,19 +13,21 @@ import NotFound from './pages/NotFound/NotFound';
 
 /* Components */ 
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
   render() {
     return (
         <div className="App">
-        <Navbar />
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/logement/:id" exact component={Logement} />
-            <Route path="/logement/*" exact component={NotFound} />
-            <Route path="/apropos" exact component={Apropos} />
-            <Route path="/*" component={NotFound} />
-        </Switch>
+            <Navbar />
+             <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/logement/:id" exact component={Logement} />
+                <Route path="/logement/*" exact component={NotFound} />
+                <Route path="/apropos" exact component={Apropos} />
+                <Route path="/*" component={NotFound} />
+            </Switch> 
+            <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+/* Librairies */ 
+import React from 'react';
+
+/* CSS */
+import "../../style.css";
+import "./Footer.css";
+
+class Footer extends React.Component {
+
+      render() {
+            return (
+                  <div className="container1240px footer">
+                       FOOTER
+                  </div>
+            )
+      }
+}
+
+export default Footer;
