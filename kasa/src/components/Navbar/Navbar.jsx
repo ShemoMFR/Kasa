a6f1c6870b13c96be9340e9fe38976c7d8925fa4
Shemo <moshe.frydmann@gmail.com>
2021-12-21 13:00:34 +0200

    page Home

--- kasa/src/components/Navbar/Navbar.jsx
+++ kasa/src/components/Navbar/Navbar.jsx
@@ -2,16 +2,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+/* logo */
+import Logo from "../../images/Logos/logo.png";
+
 /* css */
+import "../../style.css";
 import "./Navbar.css";
 
 class Navbar extends React.Component {
       render() {
             return (
-                  <nav>
-                        <Link to ='/'>Accueil</Link>
-                        <Link to ='/Apropos'>Apropos</Link>
-                  </nav>
+                  <div className="containerNavbar">
+                        <img src={Logo} alt="logo Kasa" className="logo" />
+                        <nav className="navbar">
+                              <Link className="txtNavbar" to ='/'>Accueil</Link>
+                              <Link className="txtNavbar" to ='/Apropos'>Apropos</Link>
+                        </nav>
+                  </div>
             )
       }
 }
--- kasa/src/pages/Home/Home.jsx
+++ kasa/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
+/* Librairies */ 
 import React from 'react';
+
+/* CSS */
+import ".././../style.css";
 import "./Home.css";
 
 class Home extends React.Component {
       render() {
             return (
-                  <div>
-                        TEST HOME
+                  <div className="container1400px containerHome">
+                        <div className="banniereHome">
+                              <div className="txtBanniereHome">Chez vous, partout et ailleurs</div>
+                        </div>
                   </div>
             )
             
