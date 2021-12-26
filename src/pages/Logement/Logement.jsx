7159e484e651bd8d5234aa135a0e516eb6939055
Shemo <moshe.frydmann@gmail.com>
2021-12-26 10:39:14 +0200

    caroussel OK

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,5 +1,5 @@
 /* Librairies */ 
-import React from "react";
+import React, { useRef } from "react";
 
 /* Database */ 
 import db from '../../db/db';
@@ -17,18 +17,59 @@ import Tags from "../../components/Tags/Tags";
 import Stars from "../../components/Stars/Stars";
 
 class Logement extends React.Component {
+    
+      constructor(props) {
+            super(props);
+            this.state = {
+                  logement_id: db.filter(elem => elem.id === this.props.match.params.id),   
+                  photos: [],
+                  iterator : 0
+            }
+            this.image = React.createRef();
+            this.click1 = React.createRef();
+            this.click2 = React.createRef();
+          }
 
-      state = {
-            logement_id: db.filter(elem => elem.id === this.props.match.params.id),
+      componentDidMount() {
+            this.setState({photos: [...this.state.logement_id[0].pictures]})
+
+            if (this.state.logement_id[0].pictures.length === 1) {
+                  this.click1.current.style.display = 'none';
+                  this.click2.current.style.display = 'none';
+            }
+      }
+
+      handleClickChangePhotosPrev() {
+
+             if (this.state.iterator === 0) {
+                  this.setState({iterator: this.state.photos.length - 1});
+                  this.image.current.src = `${this.state.photos[this.state.photos.length - 1]}`;
+                  return true;
+            }
+
+            this.image.current.src = `${this.state.photos[this.state.iterator - 1]}`;
+            this.setState({iterator: this.state.iterator - 1})
+      }
+
+      handleClickChangePhotosNext() {
+
+            if (this.state.iterator === this.state.photos.length - 1) {
+                  this.setState({iterator: 0});
+                  this.image.current.src = `${this.state.photos[0]}`;
+                  return true;
+            }
+
+            this.image.current.src = `${this.state.photos[this.state.iterator + 1]}`;
+            this.setState({iterator: this.state.iterator + 1})
       }
 
       render() {
             return (
                   <div className="container1240px ">
                         <div style={{position: "relative"}}>
-                              <img className='imgLogement' src={`${this.state.logement_id[0].cover}`} alt='principal logement'/>
-                              <img src={chevron} alt='chevron' className='chevronL'/>
-                              <img src={chevron} alt='chevron' className='chevronR'/>
+                              <img className='imgLogement' src={`${this.state.photos[0]}`} ref={this.image} alt='principal logement'/>
+                              <img onClick={() => this.handleClickChangePhotosPrev()} src={chevron} ref={this.click1} alt='chevron' className='chevronL'/>
+                              <img onClick={() => this.handleClickChangePhotosNext()} src={chevron} ref={this.click2} alt='chevron' className='chevronR'/>
                         </div>
                         <div className='headerLogement'>
                               <div className='headerLeft'>
