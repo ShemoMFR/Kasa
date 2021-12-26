fa3fffddae3b6c9f187ce7592239c59009f7faae
Shemo <moshe.frydmann@gmail.com>
2021-12-26 10:51:38 +0200

    page 404 finished

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,19 @@
+/* librairies */
 import React from "react";
+import { Link } from "react-router-dom";
 
 /* css */
 import "./NotFound.css";
+import '././../../style.css';
 
 class NotFound extends React.Component {
 
       render() {
             return (
-                  <div>
-                        ERREUR 404
+                  <div className='containerMain'>
+                        <h1 className='title'>404</h1>
+                        <h2 className='txt'>Page not found</h2>
+                        <Link className="link" to ='/'>Retourner sur la page d'accueil</Link>
                   </div>
             )
       }
